fffb2b19560efbfb61a27f0ffc39fc09da06e569
David Nakabaale <dnakabaa@redhat.com>
2026-09-22 11:28:57 -0400

    [COST-8093] respect CSV AllNamespaces in the manager and RBAC (#137)

--- scripts/install-cmsc.sh
+++ scripts/install-cmsc.sh
@@ -1132,7 +1132,9 @@ deploy_helm_chart() {
     # configures. Community is admission-valid and bundles DB/cache + ODF discovery.
     local sample="${project_root}/config/samples/service.costmanagement_v1alpha1_costmanagementserviceconfig_community.yaml"
 
-    local operator_ns="koku-service-operator-system"
+    # Operator install NS matches config/default kustomize (`make deploy`),
+    # not NAMESPACE (CR / Helm operands). AllNamespaces watches every NS.
+    local operator_ns="cost-onprem"
     local pull_img="${IMG:-image-registry.openshift-image-registry.svc:5000/${operator_ns}/koku-service-operator:latest}"
     local registry_host
     registry_host="$(oc get route default-route -n openshift-image-registry -o jsonpath='{.spec.host}' 2>/dev/null || true)"
